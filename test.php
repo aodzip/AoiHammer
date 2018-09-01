22916be846f7fba9961f7030d3bcd5939fa99ca4
Aodzip <aodzip@gmail.com>
2018-09-01 18:46:53 +0800

    dont require \r\n

--- test.php
+++ test.php
@@ -2,7 +2,10 @@
 $socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
 socket_connect($socket, "127.0.0.1", "8023");
 for ($i = 1; $i < 50000000; $i++) {
-    socket_write($socket, "A $i $i\r\n");
+    socket_write($socket, "A $i $i");
     $return = socket_read($socket, 16);
     echo "\r\x1b[K $i $return";
+    if ($return != "ok") {
+        die();
+    }
 }
